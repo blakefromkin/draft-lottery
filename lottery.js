fc71cd7738921c3672921e4c7de030aeadd02b81
Blake Fromkin <blakefromkin@gmail.com>
2023-03-01 16:37:53 -0500

    App fully functional

--- lottery.js
+++ lottery.js
@@ -1,89 +1,71 @@
-let express = require('Express');
-let morgan = require('morgan');
-let app = express();
+const express = require('Express');
+const morgan = require('morgan');
+const session = require("express-session");
+const store = require("connect-loki")
+const TeamsClass = require('./lib/teams.js');
 
-const ODDS = {
-  team1: 40,
-  team2: 25,
-  team3: 15,
-  team4: 10,
-  team5: 5,
-  team6: 5
-};
+const app = express();
+const LokiStore = store(session);
 
-const PICS = {
-  team1: 'image1.jpeg',
-  team2: 'image2.jpeg',
-  team3: 'image3.jpeg',
-  team4: 'image4.jpeg',
-  team5: 'image5.jpeg',
-  team6: 'image6.jpeg',
-};
-
-const LAST_PICK = 6;
-
-function createTeamsArray() {
-  for (let team in ODDS) {
-    if (ODDS[team] % 1 !== 0) {
-      for (let team in ODDS) {
-        ODDS[team] = ODDS[team] * 2;
-      }
-      break;
-    }
-  }
-
-  let teamsArray = [];
-  for (let team in ODDS) {
-    for (let count = 0; count < ODDS[team]; count += 1) {
-      teamsArray.push(team);
-    }
-  } 
-  return teamsArray;
-}
-
-function selectTeam(teamsArray) {
-  let randIdx = Math.floor(Math.random() * teamsArray.length);
-  return teamsArray[randIdx];
-}
-
-function removeTeam(team, teamsArray) {
-  return teamsArray.filter(val => val !== team);
-}
-
-function generateOrder(teamsArray) {
-  let order = [];
-  while (order.length < 6) {
-    let team = selectTeam(teamsArray);
-    teamsArray = removeTeam(team, teamsArray);
-    order.unshift(team);
-  }
-  return order;
-}
-
-let teamsArray = createTeamsArray();
-let order = generateOrder(teamsArray);
-let pickNum = LAST_PICK;
-
-// SERVER 
 app.set('view engine', 'pug');
 app.set("views", "./views");
 
-app.use(express.static('public'));
+app.use(express.static('lib'));
 app.use(morgan("common"));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  cookie: {
+    httpOnly: true,
+    maxAge: 100 * 24 * 60 * 60 * 1000, // 100 days in millseconds
+    path: "/",
+    secure: false,
+  },
+  name: "draft-lottery-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "not secure",
+  store: new LokiStore({}),
+}));
+
+// Initialize res.locals properties
+app.use((req, res, next) => {
+  res.locals.teamsManager = new TeamsClass(req.session); 
+  res.locals.order = req.session.order || res.locals.teamsManager.randomizeDraftOrder();
+  req.session.order = res.locals.order;
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('landing.pug');
 });
 
-app.get('/pick', (req, res) => {
-  let team = order[LAST_PICK - pickNum];
-  let img = PICS[team];
-  res.render('pick.pug', {img: img, pickNum: pickNum});
+app.get('/pick/:pickNum', (req, res) => {
+  let pickNum = +req.params.pickNum;
+  let teamsManager = res.locals.teamsManager;
+
+  let team = teamsManager.getTeams().find(team => {
+    return team.name === res.locals.order[teamsManager.totalTeams - pickNum];
+  });
+  let img = team.pic;
+
+  res.render('pick.pug', {img, pickNum});
   pickNum -= 1;
 });
 
 app.get('/summary', (req, res) => {
-  res.render('summary.pug', {order: order});
+  let teamsManager = res.locals.teamsManager;
+  let order = res.locals.order.map(teamName => {
+    return teamsManager.getTeams().find(team => {
+      return teamName === team.name;
+    });
+  }).reverse();
+  res.render('summary.pug', {order});
+});
+
+app.get('/reset', (req, res) => {
+  delete req.session.order;
+  res.locals.teamsManager.deleteWeighted();
+  res.redirect('/');
 });
 
 app.listen(3000, "localhost", () => {
